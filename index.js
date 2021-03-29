3ebb3f3a20d85898bb017c6db33f4e9db76e06bd
Lucas Dutra Pereira <lucas.dp.1102@gmail.com>
2021-03-29 00:23:29 -0300

    Criação da rota Layout

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/msg',function(req,res){
     res.send("Essa mensagem é automática!")
 })
 
+app.get('/layout',function(req,res){
+    res.send("Esse é o novo Layout!")
+})
+
 app.listen(3000,function(){
     console.log("Conexão Inicializada")
 })
