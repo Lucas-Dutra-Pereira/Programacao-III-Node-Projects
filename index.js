dfc925038f02945a1970c1ddc8fbfcbc9c9eb229
Lucas Dutra Pereira <lucas.dp.1102@gmail.com>
2021-04-09 19:26:26 -0300

    Configurações inicias do projeto

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require ('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function(req,res){
     res.send("Oi, sou o Sting, seja bem vindo à minha página Web")
-})
-
-
-app.get('/msg',function(req,res){
-    res.send("Essa mensagem é automática!")
-})
-
-app.get('/sobre', function(req,res){
-    res.send("Página sobre do site")
-})
-
-app.get('/layout',function(req,res){
-    res.send("Esse é o novo Layout!")
 })
 
 app.listen(3000,function(){
