df98a59faada692a9c97fb9765bf6788c875368a
Lucas Dutra Pereira <lucas.dp.1102@gmail.com>
2021-04-12 19:54:27 -0300

    Adicionando dados ao MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
+var Usuario = require('./model/usuario')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -16,17 +17,23 @@ app.get('/', function(req,res){
     res.render('index.ejs',{})
 })
 
-app.get('/usuarios', function(req,res){
-    res.render('usuarios.ejs',{usuarios: [
-        {Nome:'Lucas Dutra',Email:'lucasdp2002@outlook.com'},
-        {Nome:'Crowley',Email:'mrcrowley1985@outlook.com'}
-    ]})
-})
 app.get('/add',function(req,res){
     res.render('adiciona.ejs')
 })
 app.post('/add',function(req,res){
-    console.log("Nome: " + req.body.txtNome + " Email: " + req.body.txtEmail )
+    var usuario = new Usuario({
+        nome: req.body.txtNome,
+        email: req.body.txtEmail,
+        senha: req.body.txtSenha,
+        foto: req.body.txtFoto
+    })
+    usuario.save(function(err){
+        if(err){
+            console.log(err)
+        }else {
+            res.redirect('/');
+        }
+    })
 })
 
 app.listen(3000,function(){
